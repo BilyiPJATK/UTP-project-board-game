57f0bfbbd2387d660b3d1d2aa9e7592134a84776
BilyiPJATK <s31718@pjwstk.edu.pl>
2024-10-25 17:15:26 +0200

    connection to java

--- library.cpp
+++ library.cpp
@@ -1,6 +1,21 @@
 #include "Main.h"
+#include "ArraySumExample.h"
 
 JNIEXPORT jint JNICALL Java_Main_fun
         (JNIEnv *env, jclass jObj, jint aVal, jint bVal){
     return aVal + bVal;
+}
+
+JNIEXPORT jint JNICALL Java_ArraySumExample_calculatorSum
+        (JNIEnv *env, jclass jObj, jintArray numbers){
+
+    jint length = env->GetArrayLength(numbers);
+
+    jint *elements = env->GetIntArrayElements(numbers, NULL);
+
+    int sum = 0;
+    for (int i = 0; i < length; i++)
+        sum += elements[i];
+
+    return sum;
 }
